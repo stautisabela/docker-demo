2d81d4a37dd290e6b15706ce1b6ff834f8a9c0e1
stautisabela <stautisabela@gmail.com>
2022-12-22 09:52:27 +0100

    Created endpoint

--- docker-demo/src/main/java/com/stautisabela/dockerdemo/DockerDemoApplication.java
+++ docker-demo/src/main/java/com/stautisabela/dockerdemo/DockerDemoApplication.java
@@ -2,12 +2,19 @@ package com.stautisabela.dockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerDemoApplication {
+	
+	@GetMapping("/")
+	public String displayMessage() {
+		return "I'm in a container!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerDemoApplication.class, args);
 	}
-
 }
